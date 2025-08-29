83eb70d31bb779dd9ef834bb07b50e074749a2ac
MoEhab74 <mohamedehabswe74@gmail.com>
2025-08-29 03:11:30 +0300

    Add Search dialog

--- lib/helper/helper_dialog.dart
+++ lib/helper/helper_dialog.dart
@@ -46,3 +46,55 @@ void loadingIndicator(BuildContext context, {String message = 'Loading...'}) {
       ),
     );
   }
+
+  showSearchDialogBox(BuildContext context) {
+  showDialog(
+    context: context,
+    builder: (context) => AlertDialog(
+      content: SizedBox(
+        height: 150,
+        width: 250,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextFormField(
+              onSaved: (value) {
+                // Save the search query
+              },
+              decoration: InputDecoration(
+                hintText: 'Search...',
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.primary,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                focusedErrorBorder: OutlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Theme.of(context).colorScheme.error,
+                  ),
+                  borderRadius: BorderRadius.circular(20),
+                ),
+                border: const OutlineInputBorder(),
+              ),
+            ),
+            const SizedBox(height: 16),
+            ElevatedButton(
+              onPressed: () {
+                // Handle search action
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
--- lib/widgets/home_view_stack.dart
+++ lib/widgets/home_view_stack.dart
@@ -1,11 +1,24 @@
+import 'package:expense_tracker/cubits/expenses_cubit/expenses_cubit.dart';
 import 'package:expense_tracker/helper/constants.dart';
+import 'package:expense_tracker/helper/helper_dialog.dart';
 import 'package:expense_tracker/views/profile_view.dart';
 import 'package:expense_tracker/widgets/balance_card.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-class HomeViewStack extends StatelessWidget {
+class HomeViewStack extends StatefulWidget {
   const HomeViewStack({super.key});
+
+  @override
+  State<HomeViewStack> createState() => _HomeViewStackState();
+}
+
+class _HomeViewStackState extends State<HomeViewStack> {
+  final TextEditingController _searchController = TextEditingController();
+
+
 
   @override
   Widget build(BuildContext context) {
@@ -36,7 +49,7 @@ class HomeViewStack extends StatelessWidget {
                         Navigator.pushNamed(context, ProfileView.profileRoute);
                       },
                       child: Text(
-                        currentUser,
+                        FirebaseAuth.instance.currentUser!.email!,
                         style: TextStyle(
                           color: Theme.of(context).colorScheme.onPrimary,
                           fontSize: 20,
@@ -52,7 +65,44 @@ class HomeViewStack extends StatelessWidget {
                     Icons.search,
                     color: Theme.of(context).colorScheme.onPrimary,
                   ),
-                  onPressed: () {},
+                  onPressed: () {
+                    // Search Dialog
+                    showDialog(
+                      context: context,
+                      builder: (context) => AlertDialog(
+                        content: SizedBox(
+                          height: 150,
+                          width: 250,
+                          child: Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              SearchField(searchController: _searchController),
+                              const SizedBox(height: 16),
+                              ElevatedButton(
+                                onPressed: () {
+                                  Navigator.of(context).pop();
+                                  _searchController.clear();
+
+                                  // Handle search action
+                                  // Trigger the search method from the ExpensesCubit
+                                  BlocProvider.of<ExpensesCubit>(
+                                    context,
+                                  ).searchForExpenseByTitle(
+                                    _searchController.text,
+                                  );
+                                  // Fetch all new expenses after searching
+                                  BlocProvider.of<ExpensesCubit>(
+                                    context,
+                                  ).fetchAllExpenses();
+                                },
+                                child: const Text('OK'),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ],
             ),
@@ -65,6 +115,54 @@ class HomeViewStack extends StatelessWidget {
           child: SizedBox(width: 300, height: 200, child: BalanceCard()),
         ),
       ],
+    );
+  }
+}
+
+class SearchField extends StatelessWidget {
+  const SearchField({
+    super.key,
+    required TextEditingController searchController,
+  }) : _searchController = searchController;
+
+  final TextEditingController _searchController;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: _searchController,
+      onSaved: (value) {
+        // Save the search query
+        _searchController.text = value ?? '';
+      },
+      decoration: InputDecoration(
+        hintText: 'Search...',
+        enabledBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(
+              context,
+            ).colorScheme.primary,
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(
+              context,
+            ).colorScheme.primary,
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        focusedErrorBorder: OutlineInputBorder(
+          borderSide: BorderSide(
+            color: Theme.of(
+              context,
+            ).colorScheme.error,
+          ),
+          borderRadius: BorderRadius.circular(20),
+        ),
+        border: const OutlineInputBorder(),
+      ),
     );
   }
 }
